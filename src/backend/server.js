e153d8e47114092afa4f849af392eb4f0f568f42
Voeffray-Lucielle-EMF <143508606+Voeffray-Lucielle-EMF@users.noreply.github.com>
2025-02-26 13:14:39 +0100

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Ceci est pour un test de build UwU heheh
+// Ceci est pour un test de build UwU hehe
 
 app.use(cors());
 
